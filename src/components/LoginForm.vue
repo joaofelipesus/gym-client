a146e385e08cba01c1dd8833cf03800d61c68a5a
joaofelipe <joaofelipe1294@gmail.com>
2020-04-12 16:30:27 -0300

    Added mobile login form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,36 @@
+<template>
+  
+  <div id="login-form" class="panel is-info">
+    <p class="has-text-centered panel-heading">Bem vindo</p>
+    <div class="panel-block">
+      <form>
+        <div class="field">
+          <label class="label">Email</label>
+          <input class="input" type="email">
+        </div>
+        <div class="field">
+          <label class="label">Senha</label>
+          <input class="input" type="password">
+        </div>
+        <button class="button is-medium is-fullwidth is-success">
+          Entrar
+        </button>
+      </form>
+    </div>
+  </div>
+
+</template>
+
+<style lang="scss">
+  
+  #login-form{
+    width: 95%;
+    margin-left: 2.5%;
+    margin-top: 15%;
+    padding-bottom: 1%;
+  }
+
+  form{
+    width: 100%;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LoginForm></LoginForm>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import LoginForm from '@/components/LoginForm'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    LoginForm
   }
 }
 </script>

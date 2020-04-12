afc14d102068695c7c52fa07c193f10ee39507c3
joaofelipe <joaofelipe1294@gmail.com>
2020-04-12 16:48:24 -0300

    Ajusted login form to work in desktop too.

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,36 +1,37 @@
 <template>
   
-  <div id="login-form" class="panel is-info">
-    <p class="has-text-centered panel-heading">Bem vindo</p>
-    <div class="panel-block">
-      <form>
-        <div class="field">
-          <label class="label">Email</label>
-          <input class="input" type="email">
-        </div>
-        <div class="field">
-          <label class="label">Senha</label>
-          <input class="input" type="password">
-        </div>
-        <button class="button is-medium is-fullwidth is-success">
-          Entrar
-        </button>
-      </form>
+  <div class="columns">
+    <div id="login-form" class="column panel is-info is-fullwidth-mobile is-half-desktop is-offset-one-quarter-desktop is-paddingless-desktop">
+      <p class="has-text-centered panel-heading">Bem vindo</p>
+      <div class="panel-block">
+        <form>
+          <div class="field">
+            <label class="label">Email</label>
+            <input class="input" type="email">
+          </div>
+          <div class="field">
+            <label class="label">Senha</label>
+            <input class="input" type="password">
+          </div>
+          <button class="button is-medium-mobile is-fullwidth-mobile is-success">
+            Entrar
+          </button>
+        </form>
+      </div>
     </div>
   </div>
+  
 
 </template>
 
 <style lang="scss">
-  
+
   #login-form{
-    width: 95%;
-    margin-left: 2.5%;
-    margin-top: 15%;
-    padding-bottom: 1%;
+    margin-top: 5%;
   }
 
   form{
     width: 100%;
   }
+  
 </style>

3a93ed1d03b13b724708ea64d511bfa588e524e5
joaofelipe <joaofelipe1294@gmail.com>
2020-04-14 20:42:32 -0300

    Added unit tests that covers error_message component.

---
+++ tests/unit/shared/ErrorMessage.spec.js
@@ -0,0 +1,24 @@
+import { expect } from 'chai'
+//import LoginForm from '@/components/LoginForm.vue'
+import { mount } from '@vue/test-utils'
+import ErrorMessage from '@/components/shared/ErrorMessage'
+
+describe('Error message', () => {
+  it('doesnt render message when error is empty', () => {
+    const wrapper = mount(ErrorMessage, {
+      propsData: {
+        errors: ''
+      }
+    })
+    expect(wrapper.text()).to.not.match(/Some error/)
+  })
+
+  it('renders when errors has a value', async () => {
+    const wrapper = mount(ErrorMessage, {
+      propsData: {
+        errors: 'Some error'
+      }
+    })
+    expect(wrapper.text()).to.match(/Some error/)
+  })
+})

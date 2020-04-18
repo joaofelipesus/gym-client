e2a8a56686d1ed08633117ae1ab2e4cca4ab6725
joaofelipe <joaofelipe1294@gmail.com>
2020-04-18 09:36:34 -0300

    Implemented update from exercise.

---
+++ src/views/exercises/EditExercise.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <AdminNavbar />
+    
+    <EditExerciseForm />
+
+  </div>
+</template>
+
+
+
+<script>
+import AdminNavbar from '../../components/admin/AdminNavbar'
+import EditExerciseForm from '../../components/exercises/EditExerciseForm'
+export default {
+  name: 'NewExercise',
+  components: {
+    AdminNavbar,
+    EditExerciseForm
+  }
+}
+</script>

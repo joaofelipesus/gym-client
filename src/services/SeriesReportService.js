b54a2fe7f8c8d13038f4f6eb0352e74df3649915
joaofelipe <joaofelipe1294@gmail.com>
2020-05-04 15:27:57 -0300

    Implemented chart with weights progression from a exercise.

--- src/services/SeriesReportService.js
+++ src/services/SeriesReportService.js
@@ -6,6 +6,10 @@ export default {
   async create(series_reports){
     let response = await axios.post(`/series_reports`, { series_reports: series_reports} , { headers: store.getters['headers'] })
     return response
+  },
+
+  async progression (exercise_id){
+    return await axios.get(`/series_reports/${exercise_id}/progression`, { headers: store.getters['headers'] })
   }
 
 }
